a938a989ad9825ccd23a71078b6e7a79645fbfaf
Sangbeo113 <nguyenvuongngocsang11321@gmail.com>
2023-04-12 20:11:25 +0700

    sang

--- auth/login/index.php
+++ auth/login/index.php
@@ -29,15 +29,15 @@
 					<div class="wrap d-md-flex">
 						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
 							<div class="text w-100">
-								<h2>Welcome to login</h2>
-								<p>Don't have an account?</p>
-								<a href="../register/index.php" class="btn btn-white btn-outline-white">Sign Up</a>
+								<h2>Xin chào </h2>
+								<p>Bạn chưa có tài khoản?</p>
+								<a href="../register/index.php" class="btn btn-white btn-outline-white">Đăng kí Ngay</a>
 							</div>
 			      </div>
 						<div class="login-wrap p-4 p-lg-5">
 			      	<div class="d-flex">
 			      		<div class="w-100">
-			      			<h3 class="mb-4">Sign In</h3>
+			      			<h3 class="mb-4">Đăng nhập</h3>
 			      		</div>
 						  
 								<div class="w-100">
@@ -50,19 +50,19 @@
 			      	</div>
 							<form method="POST" class="signin-form">
 			      		<div class="form-group mb-3">
-			      			<label class="label" for="name">Username</label>
+			      			<label class="label" for="name">Tên Đăng nhập</label>
 			      			<input minlength="5" maxlength="20" name="username" type="text" class="form-control" placeholder="Username" required>
 			      		</div>
 		            <div class="form-group mb-3">
-		            	<label class="label" for="password">Password</label>
+		            	<label class="label" for="password">Mật khẩu</label>
 		              <input minlength="8" maxlength="50" name="password" type="password" class="form-control" placeholder="Password" required>
 		            </div>
 		            <div class="form-group">
-		            	<button name="submit" type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+		            	<button name="submit" type="submit" class="form-control btn btn-primary submit px-3">Đăng nhập</button>
 		            </div>
 		            <div class="form-group d-md-flex">
 		            	<div class="w-50 text-left">
-			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+			            	<label class="checkbox-wrap checkbox-primary mb-0">Ghi nhớ
 									  <input type="checkbox" checked>
 									  <span class="checkmark"></span>
 										</label>
