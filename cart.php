eea00d95fcbc48485ef8ede68a6050d973be6fd4
Dat huynh <s3777091@rmit.edu.vn>
2023-04-16 13:31:31 +0700

    sang

--- cart.php
+++ cart.php
@@ -24,7 +24,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link href="apple-touch-icon.png" rel="apple-touch-icon">
     <link href="favicon.png" rel="icon">
-    <meta name="author" content="Nghia Minh Luong">
+    <meta name="author" content="Nguyễn Vương Ngọc Sang">
     <meta name="keywords" content="Default Description">
     <meta name="description" content="Default keyword">
     <title>SKT - Trang chủ</title>
